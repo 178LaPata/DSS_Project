3985c07099ec8affd16fcbb34c89006adb9b705b
Lara Ferreira <73527417+178LaPata@users.noreply.github.com>
2023-01-07 22:25:48 +0000

    config

--- Fase3/src/database/DAOconfig.java
+++ Fase3/src/database/DAOconfig.java
@@ -1,5 +1,10 @@
 package database;
 
 public class DAOconfig {
-    
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root"; 
+    private static final String DATABASE = "DSS_Project";
+    private static final String DRIVER = "jdbc:mysql";
+    static final String URL = DRIVER+"://127.0.0.1:3306/"+DATABASE;
+    static final String CREDENTIALS = "?user="+USERNAME+"&password="+PASSWORD;
 }
--- Fase3/src/database/PilotoDAO.java
+++ Fase3/src/database/PilotoDAO.java
@@ -2,4 +2,7 @@ package database;
 
 public class PilotoDAO {
     
+
+
+    
 }
